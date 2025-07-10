1aa447b0914a32c7c75d0e70d22f7efc7698afd7
Michal92299 <michal.99967@gmail.com>
2025-07-10 11:22:11 +0200

    Update legendary-update.sh

--- LegendaryOS/Bin/legendary-update.sh
+++ LegendaryOS/Bin/legendary-update.sh
@@ -1,1 +1,124 @@
+#!/bin/bash
 
+# LegendaryOS Full Update Script
+# Autor: Michał (dla LegendaryOS)
+# Wersja: 1.3 bez emotek, z logowaniem i falującym spinnerem
+
+LOG_FILE="/tmp/legendary-update.log"
+
+# Czyszczenie logu przed uruchomieniem
+> "$LOG_FILE"
+
+# Funkcja spinnera (falujący pasek)
+spinner() {
+    local pid=$1
+    local delay=0.1
+    local frames=(
+        "[ <~       ]"
+        "[ <~~      ]"
+        "[ <~~~     ]"
+        "[ <~~~~    ]"
+        "[ <~~~~~   ]"
+        "[ <~~~~~~  ]"
+        "[ <~~~~~~~ ]"
+        "[ <~~~~~~  ]"
+        "[ <~~~~~   ]"
+        "[ <~~~~    ]"
+        "[ <~~~     ]"
+        "[ <~~      ]"
+    )
+
+    while ps -p $pid &>/dev/null; do
+        for frame in "${frames[@]}"; do
+            echo -ne "\r>>> $frame "
+            sleep $delay
+        done
+    done
+    echo -ne "\r>>> Zakończono.             \n"
+}
+
+# Nagłówek
+clear
+echo "====== [ LegendaryOS FULL UPDATE ] ======"
+echo "Log zapisywany w $LOG_FILE"
+
+### Aktualizacja firmware ###
+echo -e "\n>>> Aktualizacja firmware..."
+(sudo fwupdmgr refresh >> "$LOG_FILE" 2>&1 && \
+sudo fwupdmgr get-updates >> "$LOG_FILE" 2>&1 && \
+sudo fwupdmgr update -y >> "$LOG_FILE" 2>&1) &
+spinner $!
+
+### Aktualizacja APT ###
+echo -e "\n>>> Aktualizacja pakietów APT..."
+(sudo apt update >> "$LOG_FILE" 2>&1 && \
+sudo apt upgrade -y >> "$LOG_FILE" 2>&1) &
+spinner $!
+
+### Autoremove i autoclean ###
+echo -e "\n>>> Usuwanie niepotrzebnych pakietów..."
+(sudo apt autoremove -y >> "$LOG_FILE" 2>&1 && \
+sudo apt autoclean >> "$LOG_FILE" 2>&1) &
+spinner $!
+
+### Aktualizacja Flatpak ###
+echo -e "\n>>> Aktualizacja Flatpak..."
+(flatpak update -y >> "$LOG_FILE" 2>&1) &
+spinner $!
+
+### Aktualizacja Snap ###
+echo -e "\n>>> Aktualizacja Snap..."
+(sudo snap refresh >> "$LOG_FILE" 2>&1) &
+spinner $!
+
+### Skrypt aktualizacji aplikacji LegendaryOS ###
+echo -e "\n>>> Aktualizacja aplikacji LegendaryOS..."
+/usr/share/LegendaryOS/Bin/Update-Legendary-Apps.sh >> "$LOG_FILE" 2>&1
+
+# Zakończenie i wybór akcji
+echo -e "\n========================================="
+echo "Aktualizacja zakończona."
+echo "Log zapisany w $LOG_FILE"
+
+echo -e "\nWybierz opcję:"
+echo "(s) Shutdown"
+echo "(r) Reboot"
+echo "(l) Log out"
+echo "(e) Exit"
+echo "(t) Try again"
+echo "(o) Ponowna aktualizacja + menu"
+
+# Wczytanie pojedynczego klawisza bez ENTER
+read -n1 -s choice
+echo ""
+
+case $choice in
+    s|S)
+        echo "Wyłączanie systemu..."
+        systemctl poweroff
+        ;;
+    r|R)
+        echo "Restartowanie systemu..."
+        systemctl reboot
+        ;;
+    l|L)
+        echo "Wylogowywanie użytkownika..."
+        pkill -KILL -u "$USER"
+        ;;
+    e|E)
+        echo "Wyjście ze skryptu."
+        exit 0
+        ;;
+    t|T)
+        echo "Ponowne uruchamianie skryptu..."
+        exec "$0"
+        ;;
+    o|O)
+        echo "Ponowna aktualizacja + powrót do menu..."
+        exec "$0"
+        ;;
+    *)
+        echo "Nieznana opcja. Wyjście."
+        exit 1
+        ;;
+esac
