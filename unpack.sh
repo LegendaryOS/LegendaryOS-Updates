e4a69add027e2009a4e3084d539861940206df5c
Michal92299 <michal.99967@gmail.com>
2025-07-10 22:27:51 +0200

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -1,1 +1,13 @@
+!#/bin/bash
 
+#uprawnienia sudo
+sudo su
+
+#aktualizacja systemu
+legendary-update
+
+#usuwanie katalogu /LegendaryOS
+rm -rf /usr/share/LegendaryOS
+
+#klonowanie repo
+git clone
