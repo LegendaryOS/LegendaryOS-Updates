f9f162d6ee34ea040ec8c2d965dc6ae64ee89bc5
Michal92299 <michal.99967@gmail.com>
2025-07-10 22:37:20 +0200

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -19,4 +19,12 @@ cd LegendaryOS-Updates
 mv /LegendaryOS/Scripts/legendary-update.sh /usr/bin/
 chmod a+x /usr/bin/legendary-update.sh
 
-mv /LegendaryOS/Scripts/Kernel-Updater/etc/systemd/system/ /etc/systemd/system
+mv /LegendaryOS/Scripts/Kernel-Updater/etc/systemd/system/update-tkg-kernel.service /etc/systemd/system/
+mv LegendaryOS/Scripts/Kernel-Updater/etc/systemd/system/update-tkg-kernel.timer /etc/systemd/system/
+
+mv /LegendaryOS/Scripts/update-tkg-kernel.sh /usr/bin/
+chmod a+x /usr/bin/update-tkg-kernel.sh
+
+
+#aktualizacja <===> /usr/share/LegendaryOS/
+mv /home/$(whoami)/LegendaryOS-Updates/LegendaryOS/ /usr/share/
