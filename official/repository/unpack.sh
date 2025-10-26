d7bb085b5d4d3a95731243c6fe7f65f9d64fbd07
michal92299 <chudybyczek2137@gmail.com>
2025-10-26 18:51:50 +0100

    Update unpack.sh

--- official/repository/unpack.sh
+++ official/repository/unpack.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+sudo rm -rf /usr/share/LegendaryOS/
+sudo mv /tmp/LegendaryOS-Updates/official/repository/LegendaryOS/ /usr/share/
+cd /usr/share/LegendaryOS/SCRIPTS/
+sudo mkdir legendary
+cd legndary
+curl -L -o legendary https://github.com/LegendaryOS/legendary/releases/download/0.2/legendary
+sudo chmod a+x legendary
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/legendaryos-upgrade
