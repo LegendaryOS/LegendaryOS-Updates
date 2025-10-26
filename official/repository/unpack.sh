fa9591c5b677dc2b9e86da29d72215e792b3569b
michal92299 <chudybyczek2137@gmail.com>
2025-10-26 18:53:53 +0100

    Update unpack.sh

--- official/repository/unpack.sh
+++ official/repository/unpack.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+echo "[INFO] Starting legendaryos upgrade"
+echo "[RUN] Deleting old directory"
 sudo rm -rf /usr/share/LegendaryOS/
+echo "[RUN] Updating directories"
 sudo mv /tmp/LegendaryOS-Updates/official/repository/LegendaryOS/ /usr/share/
 cd /usr/share/LegendaryOS/SCRIPTS/
 sudo mkdir legendary
 cd legndary
+echo "[INFO] Downloading legendary tool"
 curl -L -o legendary https://github.com/LegendaryOS/legendary/releases/download/0.2/legendary
+echo "[RUN] Giving permissions"
 sudo chmod a+x legendary
 sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/legendaryos-upgrade
+echo "[INFO] Operation complete"
