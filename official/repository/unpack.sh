ca0b6e5c7c7da890a0f8d660b03c4aaefe24d403
michal92299 <chudybyczek2137@gmail.com>
2025-10-28 16:52:59 +0100

    Update unpack.sh

--- official/repository/unpack.sh
+++ official/repository/unpack.sh
@@ -8,8 +8,10 @@ cd /usr/share/LegendaryOS/SCRIPTS/
 sudo mkdir legendary
 cd legndary
 echo "[INFO] Downloading legendary tool"
-curl -L -o legendary https://github.com/LegendaryOS/legendary/releases/download/0.2/legendary
+curl -L -o legendary https://github.com/LegendaryOS/legendary/releases/download/0.4/legendary-official
+sudo mv legendary-official legendary
 echo "[RUN] Giving permissions"
 sudo chmod a+x legendary
 sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/legendaryos-upgrade
+sudo cp -r /usr/share/LegendaryOS/IMAGS/PLYMOUTH/watermark.png /usr/share/plymouth/themes/spinner/
 echo "[INFO] Operation complete"
