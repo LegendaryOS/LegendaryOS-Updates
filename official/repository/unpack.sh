9f72b3dc2a08ff4b8a4b83db845baec0dbc39ddc
michal92299 <chudybyczek2137@gmail.com>
2025-10-28 17:15:02 +0100

    Update unpack.sh

--- official/repository/unpack.sh
+++ official/repository/unpack.sh
@@ -6,12 +6,19 @@ echo "[RUN] Updating directories"
 sudo mv /tmp/LegendaryOS-Updates/official/repository/LegendaryOS/ /usr/share/
 cd /usr/share/LegendaryOS/SCRIPTS/
 sudo mkdir legendary
+sudo mkdir terminal-legend
 cd legndary
 echo "[INFO] Downloading legendary tool"
 curl -L -o legendary https://github.com/LegendaryOS/legendary/releases/download/0.4/legendary-official
 sudo mv legendary-official legendary
+cd ..
+cd terminal-legend
+curl -L -o tl-fronted https://github.com/LegendaryOS/Terminal-Legend/releases/download/0.1/tl-fronted
+curl -L -o tl-backend https://github.com/LegendaryOS/Terminal-Legend/releases/download/0.1/tl-backend
 echo "[RUN] Giving permissions"
-sudo chmod a+x legendary
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/legendary/legendary
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/terminal-legend/tl-fronted
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/terminal-legend/tl-backend
 sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/legendaryos-upgrade
 sudo cp -r /usr/share/LegendaryOS/IMAGS/PLYMOUTH/watermark.png /usr/share/plymouth/themes/spinner/
 echo "[INFO] Operation complete"
